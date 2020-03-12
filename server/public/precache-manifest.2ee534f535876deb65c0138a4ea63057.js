9fc4456ee5cbe5cd296ed543798520d8d9a6203c
Laura <lauratp02@gmail.com>
2020-03-12 22:40:10 +0100

    Deploy

---
+++ server/public/precache-manifest.2ee534f535876deb65c0138a4ea63057.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b53c5fe18bb819ecc7645c9fd4a6d88c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af195c1f02dbd5c904cf",
+    "url": "/static/css/main.a85ed7e8.chunk.css"
+  },
+  {
+    "revision": "1bab6d0122155edc1010",
+    "url": "/static/js/2.3b24f310.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.3b24f310.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af195c1f02dbd5c904cf",
+    "url": "/static/js/main.d292c2d8.chunk.js"
+  },
+  {
+    "revision": "cda345fb50b7e11c0025",
+    "url": "/static/js/runtime-main.844668cf.js"
+  }
+]);
